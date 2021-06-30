c5b7c42381be28851d295e54feb952b16f7bc209
pranavkumar389 <pk9work@gmail.com>
2021-06-30 16:23:24 +0530

    fix: style collision -- randomize production classname

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './app';
+import App from "./App";
 
 // Mount function to start up the app
 const mount = (el) => {
-    ReactDOM.render(<App />, el);
+  ReactDOM.render(<App />, el);
 };
 
 // If we are in development and in isolation call mount immediately
-if(process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
 
-    if(devRoot) {
-        mount(devRoot);
-    }
+  if (devRoot) {
+    mount(devRoot);
+  }
 }
-
 
 // We are running through container, and we should export the mount function
 export { mount };
